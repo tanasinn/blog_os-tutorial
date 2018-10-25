0e6130f4964606963d470ca16b8ca3106baf15ed
Kalle Persson <ker@qlik.com>
2018-10-25 14:12:08 +0200

    Part 3: VGA Text Mode

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,27 @@
 #![no_main]
 
 extern crate bootloader_precompiled;
+extern crate volatile;
+#[macro_use]
+extern crate lazy_static;
+extern crate spin;
 
 use core::panic::PanicInfo;
 
+#[macro_use]
+mod vga_buffer;
+
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    println!("Num {} and {}", 13, 3.14);
+    println!(".............................................");
+    panic!("Some panic message");
 
     loop {}
 }
